505742153683a90fd76d132a78d9c56e72043049
diegolopezrm <63005462+diegolopezrm@users.noreply.github.com>
2020-05-01 15:41:40 -0500

    Add files via upload

--- home_page.dart
+++ home_page.dart
@@ -1,81 +1,117 @@
-import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/providers/menu_provider.dart';
-import 'package:componentes/src/utils/icono_string_util.dart';
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+import 'package:peliculas/src/search/search_delegate.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
+  
+
+  final peliculasProvider =new PeliculasProvider();
+
   @override
   Widget build(BuildContext context) {
+
+
+    peliculasProvider.getPopulares();
+
+
     return Scaffold(
-
       appBar: AppBar(
-        title: Text('Componentes'),
-
+        centerTitle: false,
+        title: Text('Peliculas en Cines'),
+        backgroundColor: Colors.purpleAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.white,), 
+            onPressed: () {
+              showSearch(
+                context: context, 
+                delegate: DataSearch(),
+                query: ''
+              );
+            }
+          )
+        ],
       ),
-      body: _lista(),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            _swiperTarjetas(),
+            _footer(context),
+          ],
+        )
+      )
+      
+      ,
+
     );
   }
 
-  Widget _lista() {
+  Widget _swiperTarjetas() {
 
-   // print(menuProvider.opciones);
-  //  menuProvider.cargarData().then( (opciones) {
     return FutureBuilder(
+      future: peliculasProvider.getEnCines(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
 
-      future: menuProvider.cargarData(),
-      initialData: [],
-      builder: ( context, AsyncSnapshot<List<dynamic>> snapshot){
-        
-
-
-        return ListView(
-        children: _listaItems( snapshot.data, context ),
-        );
-
-
+        if ( snapshot.hasData ){
+          return CardSwiper(
+          peliculas:  snapshot.data,
+          );
+        } else {
+          return Container(
+            height: 300,
+            child: Center(
+              child: CircularProgressIndicator()
+            ),
+          );
+        }
       },
-
     );
 
+    
+    /* peliculasProvider.getEnCines();
 
-
-
+    return CardSwiper(
+      peliculas:  [1,2,3,4,5],
+    ); */
   }
 
-  List <Widget> _listaItems(List<dynamic> data, BuildContext context) {
+  Widget _footer(BuildContext context) {
 
-    final List <Widget> opciones = [];
+    return Container(
+      width: double.infinity ,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget> [
+          Container(
+            padding: EdgeInsets.only(left: 20),
+            child: Text('Populares', style: Theme.of(context).textTheme.subhead)
+          ),
+          SizedBox(height: 5),
 
-    data.forEach((opt){
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              
+              if ( snapshot.hasData ){
+                return MovieHorizontal(
+                peliculas:  snapshot.data,
+                siguientePagina: peliculasProvider.getPopulares,
+              );
+              } else {
+                return Center(child: CircularProgressIndicator());
 
-      final widgetTemp =ListTile(
-        title: Text(opt['texto']),
-        leading: getIcon(opt ['icon']),
-        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: (){
+              }
+            },
+          ),
+        ]
+      )
 
-          Navigator.pushNamed(context, opt['ruta']);
-
-
-        //  final route = MaterialPageRoute(
-        //   builder: ( context ) => AlertPage()
-        //    
-        //  );
-
-        //  Navigator.push(context, route);
-
-        },
-        
-      );
-
-      opciones..add(widgetTemp)
-              ..add( Divider() );
-             
-
-    });
-
-    return opciones;
-
+    );
   }
 }
